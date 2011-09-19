928426dc68aaa55a3d6c770c2e627ca4ba5121b5
Norwegian <norwegian@norwexuxe.dyndns.org>
2011-09-19 22:43:26 +0200

    Modificaciones para crear el paquete para pypi.python.org

--- Crawler/setup.py
+++ Crawler/setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup , find_packages
 
-setup ( name = " NorCrawler ", version = "0.1", packages = find_packages (), scripts = [' norcrawler '], install_requires = [' BeatifulSoup '], package_data = { ' pynorcrawler ': [''], }, author = " Imanol Lazkano ", author_email = "imanol.lazkano@gmail.com", description = " Crawler de Prueba ", license = "GPLv2", keywords = "", url = "", long_description = "", download_url = "" )
+setup ( name = " NorCrawler ", version = "0.1", packages = find_packages (), scripts = [' NorCrawler '], install_requires = [' BeatifulSoup '], package_data = { ' pynorcrawler ': [''], }, author = " Imanol Lazkano ", author_email = "imanol.lazkano@gmail.com", description = " Crawler de Prueba ", license = "GPLv2", keywords = "", url = "", long_description = "", download_url = "" )
